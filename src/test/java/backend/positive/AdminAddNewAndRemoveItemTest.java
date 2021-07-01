1ed489c1cadc3b2f1833b18145e34f03b9fe7900
Bogdan Nikov <nikovbogdan@gmail.com>
2021-07-01 10:52:34 +0300

    Commit my project

--- src/test/java/backend/positive/AdminAddNewAndRemoveItemTest.java
+++ src/test/java/backend/positive/AdminAddNewAndRemoveItemTest.java
@@ -19,17 +19,13 @@ public class AdminAddNewAndRemoveItemTest extends BaseTests {
     public static void addItemFromAdminPage() {
         AdminLoginPage.open();
         AdminLoginPage.login("admin", "parola123!");
-
         AdminHeader.verifyUserFullName("Milen Strahinski", "Wrong Full Name");
-
         AdminNavigationBar.selectCatalog();
         AdminNavigationBar.selectProducts();
-
         AdminPLP.addNewItem();
         AdminPLP.completeNewItemForm("Bogdan TEST PRODUCT", "<title> Bogdan </title>", "Bogdan");
         AdminPLP.verifyGreenMessageIsDisplayed();
         AdminPLP.verifyGreenMessageContainsText("Success: You have modified products!", "BUG");
-
         Browser.driver.get(Urls.USER_URL);
         UserSearchBar.searchFor("Bogdan TEST PRODUCT");
         Assert.assertTrue(UserSearchListPage.isProductDisplayed("Bogdan TEST PRODUCT"));
@@ -42,30 +38,19 @@ public class AdminAddNewAndRemoveItemTest extends BaseTests {
     public void removeItemFromAdminPage() {
         AdminLoginPage.open();
         AdminLoginPage.login("admin", "parola123!");
-
         AdminHeader.verifyUserFullName("Milen Strahinski", "Wrong Full Name");
-
         AdminNavigationBar.selectCatalog();
         AdminNavigationBar.selectProducts();
-
         AdminPLPFilter.searchForProductByName("Bogdan TEST PRODUCT");
 //        AdminPLP.delete();
 //        AdminPLP.delete(2);
         AdminPLP.deleteAll();
-
         AdminPLP.verifyGreenMessageIsDisplayed();
         AdminPLP.verifyGreenMessageContainsText("Success: You have modified products!", "BUG");
-
         Browser.driver.get(Urls.USER_URL);
         UserSearchBar.searchFor("Bogdan TEST PRODUCT");
         Assert.assertFalse(UserSearchListPage.isProductDisplayed("Bogdan TEST PRODUCT"));
 
-
     }
 
-    @Test
-    public void newMethod() {
-        AdminAddNewAndRemoveItemTest.addItemFromAdminPage();
-        Browser.driver.navigate().to("https://www.abv.bg/");
-    }
 }
