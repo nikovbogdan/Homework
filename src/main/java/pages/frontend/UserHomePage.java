658604267d1750cabece1e8e163d78313045417e
Bogdan Nikov <nikovbogdan@gmail.com>
2021-09-14 14:47:02 +0300

    Navigate to Home page validation added

--- src/main/java/pages/frontend/UserHomePage.java
+++ src/main/java/pages/frontend/UserHomePage.java
@@ -2,6 +2,7 @@ package pages.frontend;
 
 import core.Base;
 import org.openqa.selenium.By;
+import org.testng.Assert;
 import utils.Browser;
 import utils.Urls;
 
@@ -10,12 +11,15 @@ public class UserHomePage extends Base {
     private final static By MAC_BOOK_PRODUCT = By.xpath("//a[contains(text(),'MacBook')]");
     private final static By IPHONE_PRODUCT = By.xpath("//a[contains(text(),'iPhone')]");
     private final static By NAVIGATE_TO_HOME = By.id("logo");
+    private final static By CART_TOTAL = By.id("cart-total");
 
     /**
      * Browser navigates to shop.pragmatic.bg
      */
     public static void open() {
         Browser.driver.get(Urls.USER_URL);
+        Assert.assertTrue(isElementDisplayed(CART_TOTAL));
+
     }
 
     /**
